0f4aecd0caa644fb0e03797e39aef08914290146
Justin Gohde <jgohde6@gatech.edu>
2020-12-08 17:56:40 -0500

    Jest setup

---
+++ test/data.test.js
@@ -0,0 +1,19 @@
+let data = require('../data/clean/density/allStates.json');
+let stateNames = require('../data/stateLookup.json');
+
+describe('Checking data format', () => {
+  test("All %'s should be represented as decimals'", () => {
+    expect(true).toEqual(true);
+  });
+});
+
+describe('Checking data completeness', () => {
+  test("All states should be represented", () => {
+    expect(true).toEqual(true);
+  });
+
+  test("All states should have data from 1910 - 2019", () => {
+    expect(true).toEqual(true);
+  });
+
+});
